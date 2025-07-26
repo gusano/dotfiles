b5be372fdf888ba6aec4c639ff148f6c9b3d6b8e
Yvan Volochine <yvan.github@gmail.com>
2025-07-26 13:39:04 -0600

    nvim: fix codecompanion config

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,8 +1,21 @@
 return {
   "olimorris/codecompanion.nvim",
+  name = "codecompanion",
   opts = {},
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = 'gemini',
+        },
+        inline = {
+          adapter = 'gemini',
+        },
+      },
+    })
+  end
 }
